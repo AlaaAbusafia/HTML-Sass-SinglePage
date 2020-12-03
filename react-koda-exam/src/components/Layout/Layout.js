5e2a0d92be30b012b198c67c96e974fa30a6e0b8
Alaa <alaa@Ahmeds-Air.lan>
2020-12-03 13:37:36 +1300

    Adding Services and About Blocks

--- react-koda-exam/src/components/Layout/Layout.js
+++ react-koda-exam/src/components/Layout/Layout.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import Header from '../../containers/Header/header';
+import About from '../../containers/About/about';
+import Services from '../../containers/Services/services';
 import Card from '../../components/Card/card';
 import Footer from '../../containers/Footer/footer';
+
+
 
 class Layout extends Component {
 
     render(){
         return(
              <div>
-                 <section className="cards" id="team">
+                
+                <Header />
+                <section class="section-about" id="about">
+                    <About />
+                </section>
+                <Services />
+                <section className="cards" id="team">
                     <Card />
                     <Card />
                     <Card />
                     <Card />
-                 </section>
-                 <footer className="footer">
+                </section>
+                <footer className="footer">
                     <Footer />
-                 </footer>
+                </footer>
             </div>
     
         );
--- react-koda-exam/src/containers/Header/header.js
+++ react-koda-exam/src/containers/Header/header.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import Aux from './hoc/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+
+import './header.css';
 
 const header = (props) => (
-    <div></div>
+    
+    <header className="header">
+        <Toolbar />
+ 
+        <div className="header__text-box">
+            <h1 className="heading-primary">
+                <span className="heading-primary--main">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                <span className="heading-primary--sub">Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui</span>
+            </h1>
+    
+            <a href="#about" className="btn btn--blue">Find Out More</a>
+        </div>
+    </header>
 );
 
 export default header;
